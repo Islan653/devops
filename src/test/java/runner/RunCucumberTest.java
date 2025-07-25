e3637ff3a948c27b7dfe9c3ecc17efa0ab9b14df
Islan Santos <islansantosbz@gmail.com>
2025-07-25 20:49:54 -0300

    Atualizando os testes do login e correções no RunCucumberTest

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -9,16 +9,11 @@ import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features") // points to src/test/resources/features
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions") // points to the package with your steps
-@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@regressivos")
-@ConfigurationParameter(key = "cucumber.execution.dry-run", value = "true")
+@SelectClasspathResource("features") // Confirme que os .feature estão em src/test/resources/features
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefinitions") // Ou o nome completo do pacote
+@ConfigurationParameter(key = FILTER_TAGS_PROPERTY_NAME, value = "@regressivos") // Só se a tag existir
+@ConfigurationParameter(key = "cucumber.execution.dry-run", value = "false") // Execute os testes de verdade
 @ConfigurationParameter(key = "cucumber.plugin", value = "pretty, html:target/cucumber-report.html")
 @ConfigurationParameter(key = "cucumber.publish.quiet", value = "true")
-public class RunCucumberTest {
-
-
-
-}
+public class RunCucumberTest {}
